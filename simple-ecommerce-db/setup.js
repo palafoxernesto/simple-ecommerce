9ceb18b9b56e66028cc4c9f13bc384db68c8be21
Ernesto Palafox <palafox.ernesto@gmail.com>
2019-09-06 19:11:36 -0500

    Add confirmation prompt in setup [db] - Use inquirer for prompt - Use chalk for better error messages

--- simple-ecommerce-db/setup.js
+++ simple-ecommerce-db/setup.js
@@ -1,9 +1,24 @@
 'use strict'
 
 const debug = require('debug')('simple-ecommerce:db:setup')
+const inquirer = require('inquirer')
+const chalk = require('chalk')
 const db = require('./')
 
+const prompt = inquirer.createPromptModule()
 async function setup () {
+  const answer = await prompt([
+    {
+      type: 'confirm',
+      name: 'setup',
+      message: 'This will destroy your database, are you sure?'
+    }
+  ])
+
+  if (!answer.setup) {
+    return console.log('Nothing happened')
+  }
+
   const config = {
     database: process.env.DB_NAME || 'my_ecommerce',
     username: process.env.DB_USER || 'admin',
@@ -21,7 +36,7 @@ async function setup () {
 }
 
 function handleFatalError (err) {
-  console.error(err.message)
+  console.error(`${chalk.red('[fatal error]')} ${err.message}`)
   console.error(err.stack)
   process.exit(1)
 }
